b19c10344dc005c8b6cddb975d643370f06a2bbb
SD-Devv <103285649+SD-Devv@users.noreply.github.com>
2025-02-05 10:47:38 +0100

    Update app.py

--- stage 1/app.py
+++ stage 1/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -85,4 +86,5 @@ def classify_number():
     return jsonify(result), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
